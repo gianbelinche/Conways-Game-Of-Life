042b7c3f3ed988dafdbfece5af04742bae2fb635
Gianbelinche <gianluca.belinche@lambdaclass.com>
2024-04-08 18:53:00 -0300

    Add state change logic

--- src/game_logic.rs
+++ src/game_logic.rs
@@ -40,5 +40,63 @@ pub fn update_game_grid(game_grid: &GameGrid) -> GameGrid{
 
 fn get_new_state(i: usize,j: usize,grid: &GameGrid) -> State {
     // Given a cell, returns the new state based on game of lifes rules, WIP
-    grid.state[i][j]
+    let alive_neighbours = get_alive_neighbours(i,j,grid);
+    if grid.state[i][j] == State::Dead && alive_neighbours == 3 {
+        State::Alive
+    } else if grid.state[i][j] == State::Alive && (alive_neighbours == 2 || alive_neighbours == 3) {
+        State::Alive
+    } else {
+        State::Dead
+    }
+}
+
+fn get_alive_neighbours(i: usize, j: usize, grid: &GameGrid) -> u32 {
+    // Returns the number of alive neighbours for a given cell in the grid
+
+    let mut alive_neighbours = 0;
+
+    if grid.state[index_no_overflow_sub(i as i32, grid.squares) as usize][j] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[index_no_overflow_add(i as i32, grid.squares) as usize][j] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[i][index_no_overflow_add(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[i][index_no_overflow_sub(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[index_no_overflow_sub(i as i32, grid.squares) as usize][index_no_overflow_add(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[index_no_overflow_sub(i as i32, grid.squares) as usize][index_no_overflow_sub(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[index_no_overflow_add(i as i32, grid.squares) as usize][index_no_overflow_sub(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    }
+    if grid.state[index_no_overflow_add(i as i32, grid.squares) as usize][index_no_overflow_add(j as i32, grid.squares) as usize] == State::Alive {
+        alive_neighbours += 1;
+    } // TODO make this less ugly
+
+    alive_neighbours
+}
+
+fn index_no_overflow_sub(i: i32,squares: u32) -> i32 {
+    // Returns sub of i and 1 bounded by squares
+    if (i - 1) < 0 {
+        (squares - 1) as i32
+    } else {
+        i-1
+    }
+}
+
+fn index_no_overflow_add(i: i32, squares: u32) -> i32{
+    // Returns add of i and 1 bounded by squares
+    if (i + 1) > (squares - 1) as i32 {
+        0
+    } else {
+        i + 1
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod game_logic;
 const SQUARES: u32 = 16;
 #[macroquad::main("Conways Game Of Life")]
 async fn main() {
+    let mut game_grid = game_logic::create_initial_game_grid(SQUARES);
     loop {
         clear_background(WHITE);
         
-        let game_grid = game_logic::create_initial_game_grid(SQUARES);
         graphical_interface::draw_grid(&game_grid).await;
-        game_logic::update_game_grid(&game_grid);
+        game_grid = game_logic::update_game_grid(&game_grid);
         next_frame().await
     }
 }
