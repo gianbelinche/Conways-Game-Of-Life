71babd08acedc4ed166787bcc13dd2c20c9ca49b
Gianbelinche <gianluca.belinche@lambdaclass.com>
2024-04-08 16:30:11 -0300

    Initial testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const SQUARES: i16 = 16;
+#[macroquad::main("Conways Game Of Life")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        let game_size = screen_width().min(screen_height());
+        let offset_x = (screen_width() - game_size) / 2. + 10.;
+        let offset_y = (screen_height() - game_size) / 2. + 10.;
+        let sq_size = (screen_height() - offset_y * 2.) / SQUARES as f32;
+
+
+        for i in 0..SQUARES+1 {
+            draw_line(
+                offset_x,
+                offset_y + sq_size * i as f32,
+                screen_width() - offset_x,
+                offset_y + sq_size * i as f32,
+                2.,
+                LIGHTGRAY,
+            );
+        }
+
+        for i in 0..SQUARES+1 {
+            draw_line(
+                offset_x + sq_size * i as f32,
+                offset_y,
+                offset_x + sq_size * i as f32,
+                screen_height() - offset_y,
+                2.,
+                LIGHTGRAY,
+            );
+        }
+
+        next_frame().await
+    }
 }
